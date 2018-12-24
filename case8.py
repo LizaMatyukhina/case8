a9bdb505e68066b0b0e53eada995774b6e93b705
aigerimka <aigerish0603@gmail.com>
2018-12-25 05:28:43 +0700

    The first,second,third,fourth,eleventh and twelfth tasks. Correction of work.

--- case8.py
+++ case8.py
@@ -1,16 +1,110 @@
+"""Case-study #8 Data visualization
+Developers:
+Matyukhina Liza(20%), Rashidova Aigerim(60%), Romanenko Alina(0%)"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 from ru_local import *
 
-#Alina
 
-#Aigerim
+def usd_dynamics():
+    """A graph of the dynamics of the USD."""
+    usd = []
+    days = []
+    day = 1
+    with open('usd.txt') as f_in:
+        for i in f_in:
+            usd.append(float(i))
+            days.append(day)
+            day += 1
+    plt.plot(days, usd)
+    plt.show()
 
 
-#Liza
+def exchange_rates():
+    """Graphs of the dynamics of several currencies."""
+    usd = []
+    euro = []
+    aus = []
+    days = []
+    day = 1
+    with open('usd_euro_aus.txt') as f_in:
+        for i in f_in:
+            days.append(day)
+            a, b, c = i.split()
+            usd.append(float(a))
+            euro.append(float(b))
+            aus.append(float(c))
+            day += 1
+    plt.plot(days, usd, days, euro, days, aus)
+    plt.show()
 
-def number5():
+
+def finalization_of_graphs():
+    """Adding service information and displaying the grid."""
+    usd = []
+    euro = []
+    aus = []
+    days = []
+    day = 1
+    with open('usd_euro_aus.txt') as f_in:
+        for i in f_in:
+            days.append(day)
+            a, b, c = i.split()
+            usd.append(float(a))
+            euro.append(float(b))
+            aus.append(float(c))
+            day += 1
+    plt.plot(days, usd, days, euro, days, aus)
+    plt.xlabel(DAYS)
+    plt.ylabel(CURRENCY_RATE)
+    plt.title(r'$f_1=USD, f_2=euro, f_3=AUD$')
+    plt.grid(True)
+    plt.show()
+
+
+def several_graphs():
+    """Displaying multiple charts for each currency."""
+    usd = []
+    euro = []
+    aus = []
+    days = []
+    day = 1
+    with open('usd_euro_aus.txt') as f_in:
+        for i in f_in:
+            days.append(day)
+            a, b, c = i.split()
+            usd.append(float(a))
+            euro.append(float(b))
+            aus.append(float(c))
+            day += 1
+
+    # subplot 1
+    plt.subplot(221)
+    plt.plot(days, usd)
+    plt.ylabel(CURRENCY_RATE)
+    plt.title(r'$USD$')
+    plt.grid(True)
+
+    # subplot 2
+    plt.subplot(222)
+    plt.plot(days, euro, 'g')
+    plt.ylabel(CURRENCY_RATE)
+    plt.title(r'$euro$')
+    plt.grid(True)
+
+    # subplot 3
+    plt.subplot(223)
+    plt.plot(days, aus, 'r')
+    plt.ylabel(CURRENCY_RATE)
+    plt.title(r'$AUD$')
+    plt.grid(True)
+    plt.show()
+
+
+def coordinate_system():
+    """Graphs in the polar coordinate system."""
     dat = []
     dat_ = []
     money = []
@@ -54,7 +148,9 @@ def number5():
     plt.plot(dat_, money_, lw=2)
     plt.show()
 
-def number8():
+
+def labels():
+    """Using labels in the graphs."""
     dat = []
     dat_ = []
     money = []
@@ -102,7 +198,9 @@ def number8():
     plt.xlabel('Rubles')
     plt.show()
 
-def number9():
+
+def different_points():
+    """Types of points in the graphs."""
     dat = []
     dat_ = []
     money = []
@@ -148,20 +246,58 @@ def number9():
     plt.title('Currency')
     plt.show()
 
+
+def gistogram():
+    """Presentation of data in a bar graph."""
+    objects = (JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC)
+    y_pos = np.arange(len(objects))
+    performance = []
+    with open('input11.txt') as f_in:
+        for i in f_in:
+            performance.append(float(i))
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel(RUB)
+    plt.title(USD_KURS)
+    plt.show()
+
+
+def diagram():
+    """The volume of transactions for the purchase of USD and euros for rubles."""
+    data = [1036.4, 1500]
+    plt.figure(num=1, figsize=(6, 6))
+    plt.axes(aspect=1)
+    plt.title(VOLUME_OF_DEALS, size=14)
+    plt.pie(data, labels=(USD, EURO))
+    plt.show()
+
+
 def main():
-    number = input(INPUTING)
     try:
-        number = int(number)
-        if int(number) == 5:
-            number5()
-        elif int(number) == 8:
-            number8()
-        elif int(number) == 9:
-            number9()
+        n = int(input(ENTER_TASK_NUMBER))
+        if n == 1:
+            usd_dynamics()
+        elif n == 2:
+            exchange_rates()
+        elif n == 3:
+            finalization_of_graphs()
+        elif n == 4:
+            several_graphs()
+        elif n == 5:
+            coordinate_system()
+        elif n == 8:
+            labels()
+        elif n == 9:
+            different_points()
+        elif n == 11:
+            gistogram()
+        elif n == 12:
+            diagram()
         else:
             print(SORRY)
     except ValueError:
         print(WRONG_VALUES)
+
 
 if __name__ == '__main__':
     main()
